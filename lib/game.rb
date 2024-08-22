5c509d4777a68e5d3600c1c4657777428e0990cb
Chris Alexander <nc.alexander89@gmail.com>
2024-08-22 10:25:25 +1000

    Added double jump pawn

--- lib/game.rb
+++ lib/game.rb
@@ -80,11 +80,13 @@ class Game # rubocop:disable Style/Documentation,Metrics/ClassLength
     @move_pos = [@row, @col]
   end
 
-  def valid_move # rubocop:disable Metrics/AbcSize
+  def valid_move # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/MethodLength,Metrics/PerceivedComplexity
     @board.piece_positions[@piece].each do |pos| # **** this is calling piece positions
       @rules.move_positions[@piece].each do |valid_move|
         next unless @move_pos == [pos[0] + valid_move[0],
                                   pos[1] + valid_move[1]] && @board.board_array[@move_pos[0]][@move_pos[1]] == ' '
+
+        next if @move[0] == 'p' && (pos[0] != 1 && pos[0] != 6) && (@move_pos[0] - pos[0]).abs == 2
 
         @current_pos = pos
         return true
@@ -93,6 +95,11 @@ class Game # rubocop:disable Style/Documentation,Metrics/ClassLength
     puts 'Enter a valid move'
     false
   end
+
+  # def pawn
+  #       @board.piece_positions[@piece].each do |pos| # **** this is calling piece positions
+
+  # end
 
   def collision? # rubocop:disable Metrics/MethodLength,Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
     p steps = [@move_pos[0] - @current_pos[0], @move_pos[1] - @current_pos[1]]
